ddf2ccf57f8f384e5a483f531b7f0dafd5f2d768
Sasha Kondratjeva <anton79919>
2020-12-17 09:09:22 +0300

    #

---
+++ Weather/Supporting files/WeatherAPI.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by Sasha Kondratjeva on 15/12/2020.
+//  Copyright © 2020 Sasha Kondratjeva. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "ba5ee6ac7ec966af90d84980300629ca"
